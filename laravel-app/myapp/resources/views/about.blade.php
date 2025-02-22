80c2d07a94d1cd3bffa48826875306995b746d73
alverbituinrb1 <alvebituinr@gmail.com>
2025-02-22 23:57:39 +0800

    first commit

--- laravel-app/myapp/resources/views/about.blade.php
+++ laravel-app/myapp/resources/views/about.blade.php
@@ -8,11 +8,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 </head>
-<body class="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 relative overflow-hidden">
-    <div class="absolute w-60 h-60 bg-white opacity-20 rounded-full blur-3xl top-10 left-20 animate-pulse"></div>
-    <div class="absolute w-40 h-40 bg-white opacity-20 rounded-full blur-3xl bottom-10 right-20 animate-pulse"></div>
+<body class="flex items-center justify-center h-screen bg-gradient-to-r from-orange-500 to-pink-200 relative overflow-hidden">
+    <div class="absolute w-60 h-60 bg-pink opacity-20 rounded-full blur-3xl top-10 left-20 animate-pulse"></div>
+    <div class="absolute w-40 h-40 bg-yellow opacity-20 rounded-full blur-3xl bottom-10 right-20 animate-pulse"></div>
 
-    <h1 class="text-3xl font-bold text-white z-10">WELCOME TO ABOUT</h1>
+    <h1 class="text-3xl font-bold text-white z-10">ABOUT</h1>
 </body>
 
 
--- laravel-app/myapp/resources/views/contact.blade.php
+++ laravel-app/myapp/resources/views/contact.blade.php
@@ -8,11 +8,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 </head>
-<body class="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 relative overflow-hidden">
-    <div class="absolute w-60 h-60 bg-white opacity-20 rounded-full blur-3xl top-10 left-20 animate-pulse"></div>
+<body class="flex items-center justify-center h-screen bg-gradient-to-r from-yellow-500 to-black-600 relative overflow-hidden">
+    <div class="absolute w-60 h-60 bg-violet opacity-20 rounded-full blur-3xl top-10 left-20 animate-pulse"></div>
     <div class="absolute w-40 h-40 bg-white opacity-20 rounded-full blur-3xl bottom-10 right-20 animate-pulse"></div>
 
-    <h1 class="text-3xl font-bold text-white z-10">WELCOME TO CONTACT PAGE </h1>
+    <h1 class="text-3xl font-bold text-white z-10">CONTACT </h1>
 </body>
 
 
